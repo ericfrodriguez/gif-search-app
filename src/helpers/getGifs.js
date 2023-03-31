00efc522735aba612c16c6f1709e9109203b7db9
Eric Rodriguez <feric.rodriguez@gmail.com>
2023-03-31 02:52:29 -0300

    api key mode

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -3,7 +3,7 @@ import { columnizeGifs } from "./columnizeGifs";
 export const getGifs = async (category) => {
 
     try {
-        const API_KEY = import.meta.env.production.VITE_API_KEY;
+        const API_KEY = import.meta.env.VITE_API_KEY;
         const limit = 16;
     
         const url = `https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${category}&limit=${limit}`
